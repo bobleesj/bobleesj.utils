d1afacf54f0a31fbca6ca5cca8bdb366dd056f5e
Sangjoon Bob Lee <sl5400@columbia.edu>
2025-06-07 22:26:56 +0900

    chore: apply pre-commit

--- tests/test_mendeleev.py
+++ tests/test_mendeleev.py
@@ -7,5 +7,5 @@ def test_mendeleev():
     assert len(set(mendeleev.numbers.keys())) == 85
     # Check that all values are integers
     assert all(isinstance(num, int) for num in mendeleev.numbers.values())
-    # Check the value ca be retrived for a known element
+    # Check the value ca be retrieved for a known element
     assert mendeleev.numbers["H"] == 92
